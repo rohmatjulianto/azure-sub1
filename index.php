66d463c27e89a44adfcbfacdf1aab8974ee038e3
rohmatjulianto <juliantojuly@gmail.com>
2019-05-22 16:31:57 +0700

    second

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     $pass = "Sintingpisan123";
     $db = "dbo.Users";
 
+
     try {
-        $conn =  new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
     }
-?>
+
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
