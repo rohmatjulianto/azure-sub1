cae4821a54dfd205ac781cd8ee60e0268f8a4c7f
rohmatjulianto <juliantojuly@gmail.com>
2019-05-22 13:25:39 +0700

    connect db

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+    $host = "appserver-dicoding.database.windows.net";
+    $user = "rohmatjoule";
+    $pass = "Sintingpisan123";
+    $db = "dbo.Users";
+
+    try {
+        $conn =  new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
+        $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    } catch(Exception $e) {
+        echo "Failed: " . $e;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>rohmatjoule | Azure</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="jumbotron">
+        <div class="container">
+            <form action="" method="post">
+            <div class="row">
+                <div class="col-md-1">Nama</div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control form-group">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-1">Telepon</div>
+                <div class="col-md-4">
+                    <input type="text" class="form-control form-group" placeholder="ex : +628...">
+                </div>
+            </div>
+               
+            </form>
+        </div>
+    </div>
+</body>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
